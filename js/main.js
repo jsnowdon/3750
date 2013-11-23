5afdf0ff387910fcdbeb3550089208ebdc3a7528
spood <speedtouch11@gmail.com>
2013-11-23 15:50:00 -0500

    Changed json formatting

--- js/main.js
+++ js/main.js
@@ -19,16 +19,14 @@ $(document).ready(function() {
         
         // username: teacher@s.com
         // password: password
-        // http://odin.socs.uoguelph.ca:83/kirk/retrieval.php
+        // http://odin.socs.uoguelph.ca:83/kirk/Retrieval.php
         var request = $.ajax({
+            dataType: "json",
             method: "POST",
             url: "kirk/Retrieval.php",
-            data: {
-                "key" : "123kidtribute",
-                "functionName" : "Login",
-                "parameters" : {"username": username, "password": password},
-            },
-        });
+            data: "{\n\    \"key\": \"123kidtribute\",\n    \"functionName\": \"Login\",\n    \"parameters\" : {\"username\": \"" + username + "\", \"password\": \"" + password + "\"},\n}"
+            }
+        );
         request.done(function (response, textStatus, jqXHR){
         // log a message to the console
             alert("Hooray, it worked!");
@@ -109,7 +107,7 @@ function addDefaultHeader()
 function addTeacherHeader()
 {
     $("#pageHeader").empty();
-    $("#pageHeader").append("<div class=\"navbar navbar-static-top\" id=\"header\" role=\"navigation\"><div class=\"container\"><div class=\"navbar-header\" id=\"header\"><div style=\"float:right; padding-right:10px\" ><button style=\"float:right; padding:5px margin-left: 5px\" class=\"btn btn-default\"  onclick=\"logout()\" data-toggle=\"modal\">Logout</button><button style=\"float:right; padding:5px margin-left: 5px\" class=\"btn btn-default\" onclick=\"location.href='#';\">View Projects</button><button style=\"float:right; padding:5px margin-left: 5px\" class=\"btn btn-default\" onclick=\"location.href='#';\">Create Project</button><p style=\"float:right; padding:5px; padding-top:10px; color:WHITE\" id=\"usernameTag\">Username</p><p style=\"float:right; padding:5px; padding-top:10px;\" id=\"fonts\">Welcome,</p></div><h1 style=\"float:left;padding-left:10px\" id=\"fonts\">KidTribute<h1><h4 style=\"float:left;padding-left:10px\" id=\"fonts\">Helps schools help kids.<h4></div><br><br><br><div style=\"padding-left:10px\"><div class=\"input-prepend input-append\"><div class=\"btn-group\"><button id=\"dropdownBtn\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\" >Subject<span class=\"caret\"></span></button><ul class=\"dropdown-menu\" id=\"searchingDropdown\"><li><a href=\"#\">All</a></li><li><a href=\"#\">History</a></li><li><a href=\"#\">Science</a></li><li><a href=\"#\">Math</a></li><li><a href=\"#\">Visual Arts</a></li><li><a href=\"#\">Geography</a></li><li><a href=\"#\">Technology</a></li><li><a href=\"#\">Drama</a></li><li><a href=\"#\">Physical Education</a></li><li><a href=\"#\">Language Arts</a></li><li><a href=\"#\">Music</a></li></ul></div><input type=\"text\" class=\"span8 search-query\"><button type=\"submit\" class=\"btn btn-primary\">Search</button></div></div></div></div>");
+    $("#pageHeader").append("<div class=\"navbar navbar-static-top\" id=\"header\" role=\"navigation\"><div class=\"container\"><div class=\"navbar-header\" id=\"header\"><div style=\"float:right; padding-right:10px\" ><button style=\"float:right; padding:5px margin-left: 5px\" class=\"btn btn-default\"  onclick=\"logout()\" data-toggle=\"modal\">Logout</button><button style=\"float:right; padding:5px margin-left: 5px\" class=\"btn btn-default\" onclick=\"location.href='viewProject.html';\">View Projects</button><button style=\"float:right; padding:5px margin-left: 5px\" class=\"btn btn-default\" onclick=\"location.href='createProject.html';\">Create Project</button><p style=\"float:right; padding:5px; padding-top:10px; color:WHITE\" id=\"usernameTag\">Username</p><p style=\"float:right; padding:5px; padding-top:10px;\" id=\"fonts\">Welcome,</p></div><h1 style=\"float:left;padding-left:10px\" id=\"fonts\">KidTribute<h1><h4 style=\"float:left;padding-left:10px\" id=\"fonts\">Helps schools help kids.<h4></div><br><br><br><div style=\"padding-left:10px\"><div class=\"input-prepend input-append\"><div class=\"btn-group\"><button id=\"dropdownBtn\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\" >Subject<span class=\"caret\"></span></button><ul class=\"dropdown-menu\" id=\"searchingDropdown\"><li><a href=\"#\">All</a></li><li><a href=\"#\">History</a></li><li><a href=\"#\">Science</a></li><li><a href=\"#\">Math</a></li><li><a href=\"#\">Visual Arts</a></li><li><a href=\"#\">Geography</a></li><li><a href=\"#\">Technology</a></li><li><a href=\"#\">Drama</a></li><li><a href=\"#\">Physical Education</a></li><li><a href=\"#\">Language Arts</a></li><li><a href=\"#\">Music</a></li></ul></div><input type=\"text\" class=\"span8 search-query\"><button type=\"submit\" class=\"btn btn-primary\">Search</button></div></div></div></div>");
     $('#searchingDropdown li a').click(function() {
         $("#dropdownBtn").text($(this).text());
     });
