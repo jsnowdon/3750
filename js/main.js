b9fa55f4fe9b9e07e6cf0163a1d3a088baa14e65
spood <speedtouch11@gmail.com>
2013-11-23 19:49:49 -0500

    changing .val to .html

--- js/main.js
+++ js/main.js
@@ -13,9 +13,9 @@ $(document).ready(function() {
         var name = readCookie("name");
         var email = readCookie("email");
         var schoolID = readCookie("schoolid");
-        $("#nameField").val(name);
-        $("#emailField").val(email);
-        $("#schoolidField").val(schoolID);
+        $("#nameField").html(name);
+        $("#emailField").html(email);
+        $("#schoolidField").html(schoolID);
     }
     
     $('#targetLoginForm').submit(function( event ) {
