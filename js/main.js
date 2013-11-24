ee51f7f8586aa82c3a0a48893d1015611940c713
spood <speedtouch11@gmail.com>
2013-11-23 19:45:44 -0500

    hacky implementation for adding teacher name, email, and schoolid to the create project page

--- js/main.js
+++ js/main.js
@@ -9,6 +9,15 @@ $(document).ready(function() {
         addDefaultHeader();
     }
     
+    if($("#schoolidField") != null) {
+        var name = readCookie("name");
+        var email = readCookie("email");
+        var schoolID = readCookie("schoolid");
+        $("#nameField").val(name);
+        $("#emailField").val(email);
+        $("#schoolidField").val(schoolID);
+    }
+    
     $('#targetLoginForm').submit(function( event ) {
         event.preventDefault();
         var username = $("#usernameInput").val();
